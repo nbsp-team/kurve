7ceadd07f9f765c183f70f9103f2d8ae5207a80a
Dimorinny <didika914@gmail.com>
2015-06-05 18:48:37 +0300

    again fix

--- public_html/js/views/Game.js
+++ public_html/js/views/Game.js
@@ -103,18 +103,8 @@ define([
         keyDown: function (e) {
 
             switch(e.keyCode) {
-                case 32:
-                    this.playPause();
-                    break;
 
                 case this.Q_BUTTON:
-                    if(this.leftRepeat) break;
-                    this.leftRepeat = true;
-                    Api.sendKeyEvent(true, false);
-                    this.field.leftDown(this.myId);
-                    e.preventDefault();
-                    break;
-
                 case this.LEFT_BUTTON:
                     if(this.leftRepeat) break;
                     this.leftRepeat = true;
@@ -124,18 +114,11 @@ define([
                     break;
 
                 case this.W_BUTTON:
-                    if(this.leftRepeat) break;
-                    this.leftRepeat = true;
-                    Api.sendKeyEvent(true, false);
-                    this.field.leftDown(this.myId);
-                    e.preventDefault();
-                    break;
-
                 case this.RIGHT_BUTTON:
-                    if(this.leftRepeat) break;
-                    this.leftRepeat = true;
-                    Api.sendKeyEvent(true, false);
-                    this.field.leftDown(this.myId);
+                    if(that.rightRepeat) break;
+                    that.rightRepeat = true;
+                    Api.sendKeyEvent(false, false);
+                    that.field.rightDown(that.myId);
                     e.preventDefault();
                     break;
             }
@@ -144,28 +127,14 @@ define([
         keyUp: function(e) {
 
             switch(e.keyCode) {
-
                 case this.Q_BUTTON:
-                    this.leftRepeat = false;
-                    Api.sendKeyEvent(true, true);
-                    this.field.leftUp(this.myId);
-                    e.preventDefault();
-                    break;
-
                 case this.LEFT_BUTTON:
                     this.leftRepeat = false;
                     Api.sendKeyEvent(true, true);
                     this.field.leftUp(this.myId);
                     e.preventDefault();
                     break;
-
                 case this.W_BUTTON:
-                    this.rightRepeat = false;
-                    Api.sendKeyEvent(false, true);
-                    this.field.rightUp(this.myId);
-                    e.preventDefault();
-                    break;
-
                 case this.RIGHT_BUTTON:
                     this.rightRepeat = false;
                     Api.sendKeyEvent(false, true);
